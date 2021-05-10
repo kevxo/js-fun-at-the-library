b7c7004533ae61bb9287d139a1c728191ce1462c
Kevin Cuadros <kev040798@gmail.com>
2021-05-10 17:29:48 -0400

    Finish iteration 2 shelf with test

--- src/shelf.js
+++ src/shelf.js
@@ -4,10 +4,39 @@ function shelfBook(book, books) {
   }
 }
 
+function unshelfBook(title, books) {
+  for (var i = 0; i < books.length; i++) {
+    if (books[i].title === title) {
+      books.splice(i, 1);
+    }
+  }
+}
+
+
+function listTitles(books){
+  titles = [];
+
+  for (var i = 0; i < books.length; ++i){
+    titles.push(books[i].title)
+  }
+
+  return titles.join(', ');
+}
+
+function searchShelf(books, title){
+ var titles = listTitles(books);
+
+  if (titles.includes(title)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
