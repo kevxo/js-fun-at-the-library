e2e89142f6156f6df1840203a97b774c9a71624c
Kevin Cuadros <kev040798@gmail.com>
2021-05-12 16:05:33 -0400

    Finish extension exercise Librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,32 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(name, morning = false){
+
+    if (morning == true){
+      return `Good morning, ${name}!`
+    } else{
+      return `Hello, ${name}!`
+    }
+  }
+
+  findBook(bookName){
+    var fantasyBooks = this.library.shelves.fantasy
+
+    for (var i = 0; i < fantasyBooks.length; i++) {
+      if ( fantasyBooks[i].title == bookName){
+        fantasyBooks.splice(i, 1)
+        return `Yes, we have ${bookName}`
+      }
+    }
+    return `Sorry, we do not have ${bookName}`
+  }
+
+  calculateLateFee(days) {
+    return Math.round(0.25 * days)
   }
 }
 
